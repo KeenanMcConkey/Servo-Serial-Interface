4721585ee58528c514b224a020a55a8f8a2ce53c
KeenanMcConkey <k.t.mcconkey@gmail.com>
2018-09-04 15:50:23 +0200

    Add initial serial connection

--- SerialWebUI.py
+++ SerialWebUI.py
@@ -1,27 +1,54 @@
-# Set up a local web interface to take data from using Flask
+# SerialWebUI handles hosting of HTML web interface and HTTP query parsing
+# while also connecting to the M Drive servo when necessary
+
+# Global variables
+servoAngle = 0
+
+# Create a web interface to take data from using Flask
 from flask import Flask, render_template, request, url_for
 app = Flask(__name__)
 
-# Index
+## Index page
 @app.route('/')
 def index():
     return render_template('mdrive.html')
 
-# Incremental form submission
+## Incremental form submission
 @app.route('/handleIncremental', methods=['POST'])
 def handleIncremental():
     dir = request.form['dir']
     cts = request.form['cts']
     incAngle = request.form['incAngle']
     stopAngle = request.form['stopAngle']
+    updateServo(dir, cts, incAngle, stopAngle)
     return render_template('mdrive.html')
 
-# Specific form submission
+## Specific form submission
 @app.route('/handleSpecific', methods=['POST'])
 def handleSpecific():
-    angle = request.form['angle']
+    nextAngle = request.form['angle']
+    updateServo(nextAngle)
     return render_template('mdrive.html')
 
-# Automatically run in debug mode
+## Automatically run in debug mode
 if __name__ == '__main__':
     app.run(debug = True)
+
+# Connect to M Drive servo using PySerial
+import serial
+import io
+
+## Update M Drive servo with specific anglew
+def updateServo(nextAngle):
+    ## Create serial connection with servo
+    ser = serial.Serial()
+    ser.baudrate = 9600
+    ser.port = 'COM8'
+    ser.open()
+
+    serIO = io.textIOWrapper(io.buffredRWPair(ser, ser))
+    serIO.write("mr ")
+    serIO.write(nextAngle * (51200 / 360))
+    serIO.write("\n")
+    serIO.flush()
+    ser.close()

a1dcc5f25514ee57276cc13647129fca4979e281
KeenanMcConkey <k.t.mcconkey@gmail.com>
2018-09-04 19:23:38 +0200

    Working commit with Mac OS

--- SerialWebUI.py
+++ SerialWebUI.py
@@ -1,8 +1,17 @@
-# SerialWebUI handles hosting of HTML web interface and HTTP query parsing
-# while also connecting to the M Drive servo when necessary
+#!/usr/bin/env python
 
 # Global variables
-servoAngle = 0
+servoAngle = 0.0
+
+# Create serial connection with Servo using PySerial
+import serial
+import io
+
+ser = serial.Serial('/dev/cu.SLAB_USBtoUART')
+print(" * Port: {}".format(ser.port))
+print(" * Baudrate: {}".format(ser.baudrate))
+serIO = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
+serIO.write("fd")
 
 # Create a web interface to take data from using Flask
 from flask import Flask, render_template, request, url_for
@@ -20,13 +29,19 @@ def handleIncremental():
     cts = request.form['cts']
     incAngle = request.form['incAngle']
     stopAngle = request.form['stopAngle']
+
+    if (incAngle != ""):
+        incAngle = float(incAngle)
+    if (stopAngle != ""):
+        stopAngle = float(stopAngle)
+
     updateServoIncremental(dir, cts, incAngle, stopAngle)
     return render_template('mdrive.html')
 
 ## Specific form submission
 @app.route('/handleSpecific', methods=['POST'])
 def handleSpecific():
-    nextAngle = request.form['angle']
+    nextAngle = float(request.form['angle'])
     updateServoSpecific(nextAngle)
     return render_template('mdrive.html')
 
@@ -34,29 +49,18 @@ def handleSpecific():
 if __name__ == '__main__':
     app.run(debug = True)
 
-# Connect to M Drive servo using PySerial
-import serial
-import io
-
-## Update M Drive servo with specific anglew
+## Update M Drive servo with specific angle
 def updateServoSpecific(nextAngle):
-    ## Create serial connection with servo
-    ser = serial.Serial()
-    ser.baudrate = 9600
-    ser.port = 'COM8'
-    ser.open()
+    global servoAngle
+    if (servoAngle == nextAngle):
+        return
+    toWrite = "mr {} \n".format(nextAngle * (51200.0 / 360.0))
 
-    serIO = io.textIOWrapper(io.buffredRWPair(ser, ser))
-    serIO.write("mr ")
-    serIO.write(nextAngle * (51200 / 360))
-    serIO.write("\n")
+    serIO.write(toWrite)
     serIO.flush()
-    ser.close()
+
+    servoAngle = nextAngle
 
 def updateServoIncremental(dir, cts, incAngle, stopAngle):
-    ## Create serial connection with servo
-    ser = serial.Serial()
-    ser.baudrate = 9600
-    ser.port = 'COM8'
-    ser.open()
-    ser.close()
+    serIO.write("MR 51200\n")
+    ser.flush()
